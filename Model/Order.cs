e522b0aeb26448d1ae66147f20a4ad239603206d
ANGELOV-JAMES <85118303+ANGELOV-JAMES@users.noreply.github.com>
2022-08-18 23:02:25 +0800

    Update Order.cs

--- Model/Order.cs
+++ Model/Order.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.AspNetCore.StaticFiles;
 using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
@@ -29,7 +29,7 @@ namespace BackEnd.Model
             DataTable dt = DBHelper.ExecuteTable("SELECT *  FROM Order WHERE OrderID = :OrderID",
                 new OracleParameter(":OrderID", ID)
                 );
-            if (dt.Rows.Count() > 0)
+            if (dt.Rows.Count > 0)
             {
                 DataRow dr = dt.Rows[0];
                 order = dr.DtToModel<Order>();
@@ -41,7 +41,7 @@ namespace BackEnd.Model
         public static List<Order> GetAllList()
         {
             List<Order> list = new List<Order>();
-            DataTable dt = DBHelper.ExecuteTable("SELECT * FROM Order",);
+            DataTable dt = DBHelper.ExecuteTable("SELECT * FROM Order");
             foreach (DataRow dr in dt.Rows)
             {
                 list.Add(dr.DtToModel<Order>());
@@ -90,7 +90,7 @@ namespace BackEnd.Model
 
         /*根据状态列出订单信息，预留的可能使用的接口*/
         /*status取值 ed是已经完成 ing是正在进行*/
-        public static List<Room> ListByOrederStatus(string status)
+        public static List<Order> ListByOrederStatus(string status)
         {
             List<Order> list = new List<Order>();
             DataTable dt = DBHelper.ExecuteTable("SELECT * FROM Order WHERE OrederStatus = :OrederStatus",
@@ -108,23 +108,23 @@ namespace BackEnd.Model
 
         /*新创建的订单一定是进行中，所以参数中没有订单状态*/
         /*创建合法性问题没考虑订单ID的问题，只考虑了该房间是否仍然有订单处于进行状态*/
-        public static int CreateOrder(string OID, string RID,string CID , string starttime, string endtime ,string price)
+        public static int CreateOrder(string OID, string RID, string CID, string starttime, string endtime, string price)
         {
             /*考察订单合法性，确定房间是否被重复定下，假设订单ID这种东西*/
             List<Order> list = ListByRoom(RID);
             string ing = "ing";
             DataTable dt = DBHelper.ExecuteTable("SELECT * FROM Order WHERE RoomID = :RoomID AND OrederStatus= :OrederStatus",
               new OracleParameter(":RoomID", RID),
-              new OracleParameter(":OrederStatus", ing),
+              new OracleParameter(":OrederStatus", ing)
               );
 
-            if (dt.Rows.Count() = 0)
+            if (dt.Rows.Count == 0)
             {
                 return DBHelper.ExecuteNonQuery("INSERT INTO Order(OrderID,RoomID,CustomerID,StartTime,EndTime,OrederStatus,Violation,Amount) VALUES(:OrderID,:RoomID,:CustomerID,:StartTime,EndTime,OrederStatus,Violation,Amount)",
                     new OracleParameter(":OrderID", OID),
                     new OracleParameter(":RoomID", RID),
                     new OracleParameter(":CustomerID", CID),
-                    new OracleParameter(":StartTime", starttime)
+                    new OracleParameter(":StartTime", starttime),
                     new OracleParameter(":EndTime", endtime),
                     new OracleParameter(":OrederStatus", ing),
                      new OracleParameter(":Violation", null),
