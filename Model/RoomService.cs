06baf625d0c09202f37764a9e097ca489939fbb7
ANGELOV-JAMES <85118303+ANGELOV-JAMES@users.noreply.github.com>
2022-08-19 00:21:30 +0800

    Update RoomService.cs

--- Model/RoomService.cs
+++ Model/RoomService.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.AspNetCore.StaticFiles;
 using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
@@ -13,6 +13,7 @@ namespace BackEnd.Model
     {
         public string RoomID { set; get; }//PK
         public string Time { set; get; }//PK
+        public string Type { set; get; }
         public string Remark { set; get; }
         public string Amount { set; get; }
         public string Status { set; get; }
@@ -33,14 +34,15 @@ namespace BackEnd.Model
             return room_service;
         }
 
-        public static int AddRoomService(string roomid, string time, string remark, string amount, string status, string employee_id)
+        public static int AddRoomService(string roomid, string time, string type, string remark, string amount, string status, string employee_id)
         {
             RoomService room_service = Find(roomid, time);
             if (room_service == null)
             {
-                return DBHelper.ExecuteNonQuery("INSERT INTO ROOMSERVICE(RoomID,Time,Remark,Amount,Status,EmployeeID) VALUES(:RoomID,:Time,:Remark,:Amount,:Status,:EmployeeID)",
+                return DBHelper.ExecuteNonQuery("INSERT INTO ROOMSERVICE(RoomID,Time,Type,Remark,Amount,Status,EmployeeID) VALUES(:RoomID,:Time,:Type,:Remark,:Amount,:Status,:EmployeeID)",
                     new OracleParameter(":RoomID", roomid),
                     new OracleParameter(":Time", time),
+                    new OracleParameter(":Type", type),
                     new OracleParameter(":Remark", remark),
                     new OracleParameter(":Amount", amount),
                     new OracleParameter(":Status", status),
@@ -83,6 +85,20 @@ namespace BackEnd.Model
             return list;
         }
 
+        public static List<RoomJobInfo> GetUndoneJobInfo(string employee_id)
+        {
+            List<RoomJobInfo> list = new List<RoomJobInfo>();
+            DataTable dt = DBHelper.ExecuteTable("SELECT RoomID , Time, Type FROM ROOMSERVICE WHERE Status = :Status AND EmployeeID = :EmployeeID",
+                new OracleParameter(":Status", "UnDone"),
+                new OracleParameter(":EmployeeID", employee_id)
+                );
+            foreach (DataRow dr in dt.Rows)
+            {
+                list.Add(dr.DtToModel<RoomJobInfo>());
+            }
+            return list;
+        }
+
         /*public static List<RoomService> ListByRoom(string ID)
         {
             List<RoomService> list = new List<RoomService>();
@@ -95,5 +111,12 @@ namespace BackEnd.Model
             }
             return list;
         }*/
+    }
+    public class RoomJobInfo
+    {
+        public string RoomID { set; get; }//PK
+        public string Time { set; get; }//PK
+        public string Type { set; get; }
+
     }
 }
