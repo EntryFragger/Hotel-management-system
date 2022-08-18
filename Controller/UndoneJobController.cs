30290e0c2d1f1d1ba2210170b8d7fe6bae9766e3
ANGELOV-JAMES <85118303+ANGELOV-JAMES@users.noreply.github.com>
2022-08-19 00:25:41 +0800

    Controller类
    新建了第一个controller类，文档中的第二个，获取当前任未完成的任务

---
+++ Controller/UndoneJobController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BackEnd.Utility;
+using BackEnd.Model;
+
+namespace BackEnd.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UndoneJobController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetUndoneJob()
+        {
+            string id = "23";
+            List<RoomJobInfo> list = RoomService.GetUndoneJobInfo(id);
+            if (list.Count > 0)
+            {
+                return Ok(list);
+            }
+            else
+            {
+                return NotFound("不存在未完成的房间订单");
+            }
+        }
+    }
+}
