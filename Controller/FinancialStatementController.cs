fb75f61b3ca10ea547c6a4e7a48db1e349128189
LJCARG <83816521+LJCARG@users.noreply.github.com>
2022-08-21 11:06:02 +0800

    Add files via upload

--- Controller/FinancialStatementController.cs
+++ Controller/FinancialStatementController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BackEnd.Model;
-using BackEnd.Utility;
+using BackEnd.DBUtility;
 using Oracle.ManagedDataAccess.Client;
 
 namespace BackEnd.Controllers
@@ -50,7 +50,7 @@ namespace BackEnd.Controllers
         {
             try
             {
-                long statementID = FinancialStatement.NextStatementID();
+                string statementID = FinancialStatement.NextStatementID();
                 EmployeeInforToken user = JWTHelper.GetUsers(tokenValue);
                 FinancialStatement.Add(user.ID, statementID, statementContent, amount, state);
                 return Ok("提交财务报单成功");
@@ -58,6 +58,58 @@ namespace BackEnd.Controllers
             catch (OracleException oe)
             {
                 return BadRequest("数据库请求出错" + oe.Number.ToString());
+            }
+        }
+        /// <summary>
+        /// /*审批财物报单*/
+        /// </summary>
+        /// <param name="statement_id"></param>
+        /// <param name="statement_content"></param>
+        /// <param name="amount"></param>
+        /// <param name="state"></param>
+        /// <param name="token_value"></param>
+        /// <returns></returns>
+        public IActionResult FinicalStatement_Create(string statement_id, string statement_content, long amount, string state, string token_value)
+        {
+            EmployeeInforToken user = JWTHelper.GetUsers(token_value);
+            if (user.Department != "Finance")
+            {
+                return BadRequest("权限不符");
+            }
+            if (statement_id.Trim().Length == 0 || statement_content.Trim().Length == 0 || amount < 0 || state.Trim().Length == 0)
+            {
+                return BadRequest("输入信息不完整");
+            }
+            int issuccess = FinicalStatement.Add(statement_id, statement_content, amount, state);
+            if (issuccess != -1)
+            {
+                return Ok("财务报单创建成功");
+            }
+            else
+            {
+                return NotFound("财务报单创建失败");
+            }
+        }
+        /// <summary>
+        /// 获取所有财务报单，面向财务报
+        /// </summary>
+        /// <param name="token_value"></param>
+        /// <returns></returns>
+        public IActionResult GetFinancialStatement(string token_value)
+        {
+            EmployeeInforToken user = JWTHelper.GetUsers(token_value);
+            if (user.Department != "Finance")
+            {
+                return BadRequest("权限不符");
+            }
+            List<FinancialStatement> list = FinancialStatement.GetList();
+            if (list != null)
+            {
+                return Ok(list);
+            }
+            else
+            {
+                return NotFound("不存在财务报单");
             }
         }
     }
