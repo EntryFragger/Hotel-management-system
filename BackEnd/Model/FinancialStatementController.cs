abe28d68840ba976fb3b75c0be0ce04a3fbd0917
Why Not <83454355+skl523@users.noreply.github.com>
2022-08-29 00:55:32 +0800

    解决外码约束问题

---
+++ BackEnd/Model/FinancialStatementController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackEnd.Model;
+using BackEnd.Utility;
+using Oracle.ManagedDataAccess.Client;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FinancialStatementController : ControllerBase
+    {
+        /// <summary>
+        /// 获取当前人的财务报单
+        /// </summary>
+        /// <param name="tokenValue">token</param>
+        /// <returns>相应人的财务报单</returns>
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetFinancialStatement(string tokenValue)
+        {
+            try
+            {
+                EmployeeInforToken user = JWTHelper.GetUsers(tokenValue);
+                List<FinancialStatement> financialStatement = FinancialStatement.GetList(user.ID);
+                return Ok(new JsonResult(financialStatement));
+            }
+            catch (OracleException oe)
+            {
+                return BadRequest("数据库请求出错" + oe.Number.ToString());
+            }
+
+        }
+        /// <summary>
+        /// 提交财务报表
+        /// </summary>
+        /// <param name="tokenValue">token</param>
+        /// <param name="statementContent">报单内容</param>
+        /// <param name="amount">金额</param>
+        /// <param name="state">状态</param>
+        /// <returns>返回报单是否提交成功的信息</returns>
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult SubmitFinancialStatement(string tokenValue, string statementContent, float amount)
+        {
+            try
+            {
+                long statementID = FinancialStatement.NextStatementID();
+                EmployeeInforToken user = JWTHelper.GetUsers(tokenValue);
+                string state = "pending";
+                FinancialStatement.Add(user.ID, statementID, statementContent, amount, state);
+                return Ok("提交财务报单成功");
+            }
+            catch (OracleException oe)
+            {
+                return BadRequest("数据库请求出错" + oe.Number.ToString());
+            }
+        }
+        /// <summary>
+        /// 获取所有财务报单，面向财务部
+        /// </summary>
+        /// <param name="token_value">token</param>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetAllFinancialStatement(string token_value)
+        {
+            EmployeeInforToken user = JWTHelper.GetUsers(token_value);
+            if (user.Department != "Finance")
+            {
+                return BadRequest("权限不符");
+            }
+            List<FinancialStatement> list = FinancialStatement.GetListAll();
+            if (list != null)
+                return Ok(new JsonResult(list));
+            else
+                return NotFound("不存在财务报单信息");
+        }
+        /// <summary>
+        /// 财务部调用审批财务报单，会将财务报单的状态从未通过改为通过
+        /// </summary>
+        /// <param name="token_value">token</param>
+        /// <param name="sID">要审批的财务报单ID</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public IActionResult ApprovalFinancialStatement(string token_value, long sID)
+        {
+            EmployeeInforToken user = JWTHelper.GetUsers(token_value);
+            if (user.Department != "Finance")
+            {
+                return BadRequest("权限不符");
+            }
+            FinancialStatement fs = FinancialStatement.Find(sID);
+            if(fs==null)
+            {
+                return NotFound("不存在财务报单");
+            }
+            else if(fs.State=="passed")
+            {
+                return BadRequest("请勿重复审批");
+            }
+            else
+            {
+                FinancialStatement.Change_FinicalStatement_Status(sID);
+                string type = "expenses";
+                Account.CreateAccount(Account.NextID(), DateTime.Now.ToLongDateString(), fs.Amount, type);
+                return Ok("审批成功");
+            }
+        }
+    }
+}
---
+++ BackEnd/Model/PurchaseController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BackEnd.Utility;
+using BackEnd.Model;
+
+namespace BackEnd.Controller
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PurchaseController : ControllerBase
+    {
+        /// <summary>
+        /// 获取所有的货物收购信息
+        /// </summary>
+        /// <param name="token_value">token</param>
+        /// <returns>所有货物的收购信息</returns>
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        /*获取所有的货物收购信息*/
+        public IActionResult GetAllPurchase(string token_value)
+        {
+            EmployeeInforToken user = JWTHelper.GetUsers(token_value);
+            if (user.Department != "Logistics")
+            {
+                return BadRequest("权限不符");
+            }
+            List<Purchase> list = Purchase.GetAllList();
+            if (list != null)
+            {
+                return Ok(new JsonResult(list));
+            }
+            else
+            {
+                return NotFound("不存在采购信息");
+            }
+        }
+        /// <summary>
+        /// 创建货物收购信息,如果创建成功，同时添加收支信息
+        /// </summary>
+        /// <param name="goods_name">商品名称</param>
+        /// <param name="quantity">数量</param>
+        /// <param name="price">价格</param>
+        /// <param name="date">日期</param>
+        /// <param name="unit">单位</param>
+        /// <param name="token_value">token</param>
+        /// <returns>创建是否成功的提示</returns>
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public IActionResult Purchase_Create(string goods_name, string quantity, float price, string date, string unit, string token_value)
+        {
+            EmployeeInforToken user = JWTHelper.GetUsers(token_value);
+            if (user.Department != "Logistics")
+            {
+                return BadRequest("权限不符");
+            }
+            if (goods_name.Trim().Length == 0 || quantity.Trim().Length == 0 || date.Trim().Length == 0 || price < 0 || unit.Trim().Length == 0)
+            {
+                return BadRequest("输入信息不完整");
+            }
+            long p_id = Purchase.NextID();
+            int issuccess = Purchase.CreatePurchase(p_id, goods_name, unit, quantity, price, date);
+            if (issuccess != -1)
+            {
+                /*这里要取到最大的一个收获订单号，没有就取0*/
+                long account_id = Account.NextID();
+                /*随着收购，要对收支信息做出对应修改*/
+                Account.CreateAccount(account_id, date, price, "expenses");
+                return Ok("收购信息创建成功");
+            }
+            else
+            {
+                return NotFound("收购信息创建失败");
+            }
+        }
+    }
+}
